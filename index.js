7364b4b44479b7f1a30332ee76bbfa6480ea77e9
Yacine-sinapayen <yacine.sinapayen@hotmail.fr>
2021-04-26 14:25:21 +0200

    styles.css.bug et fonctionnalité form ok

---
+++ index.js
@@ -0,0 +1,43 @@
+//Step 1
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const sendMail = require('./mail');
+const { log } = console;
+const app = express();
+
+const PORT = 8080; 
+
+//step 2
+//data parsing
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+
+//email, subject, text
+
+app.post('/email', (req, res) => {
+    const { subject, email, text } = req.body;
+    log('Data: ', req.body);
+
+    sendMail(email, subject, text, function(err, data) {
+        if (err) {
+            log('ERROR: ', err);
+            return res.status(500).json({ message: err.message || 'Internal Error' });
+        }
+        log('Email sent!!!');
+        return res.json({ message: 'Email sent!!!!!' });
+    });
+});
+
+
+
+app.use( express.static(__dirname + '/integration'));
+//Render home page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "integration"));
+});
+
+
+app.listen (PORT, () => log('Server is starting on PORT,',8080));
--- mail.js
+++ mail.js
@@ -1,31 +1,33 @@
-// const nodemailer = require ('nodemailer');
-// const mailGun = require ('nodemailer-mailgun-transport')
+const nodemailer = require ('nodemailer');
+const mailGun = require ('nodemailer-mailgun-transport')
 
-// const auth = {
-//     auth:{
-//         api_key:'',
-//         domain:''
-//     }
-// };
+const auth = {
+    auth:{
+        api_key: process.env.API_KEY,
+        domain: process.env.DOMAIN
+    }
+};
 
-// const transporter = nodemailer.createTransport(mailGun(auth));
 
-// //step 4
-// const sendMail = (email, subject, text, cb) => {
-//     const mailOptions = {
-//         from: email,
-//         to: 'dk.energy.recrutement@gmail.com',
-//         subject,
-//         text
-//     };
+const transporter = nodemailer.createTransport(mailGun(auth));
+
+//step 4
+const sendMail = (email, subject, text, cb) => {
+    const mailOptions = {
+        from: email,
+        to: 'dk.energy.recrutement@gmail.com',
+        subject,
+        text
+    };
     
-//     transporter.sendMail(mailOptions, function(error, data){
-//         if(error){
-//            return cb(error, null);
-//         }
-//             return cb(null, data);    
-//     });
-// }
+    transporter.sendMail(mailOptions, function(err, data){
+        if(err){
+           return cb(err, null);
 
-// module.exports = sendMail;
+        }
+            return cb(null, data);    
+    });
+} 
+
+module.exports = sendMail;
 
