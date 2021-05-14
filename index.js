b3e796566a48160e18ab3d88aa09ed63f3a423e8
Yacine-sinapayen <yacine.sinapayen@hotmail.fr>
2021-05-14 14:24:43 +0200

    test 5dpl

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/email', (req, res) => {
 
 
 
-app.use( express.static(__dirname + '/srci'));
+app.use( express.static(__dirname + '/srci/style.css'));
 //Render home page
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/index.html"));
