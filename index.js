f8959ba5f9e862621807c756e2d9476dbb38e847
Yacine-sinapayen <yacine.sinapayen@hotmail.fr>
2021-05-20 14:37:28 +0200

    test form

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-//Step 1
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const sendMail = require('./app/mail');
-const { log } = console;
-const app = express();
+// //Step 1
+// require('dotenv').config();
+// const express = require('express');
+// const path = require('path');
+// const sendMail = require('./mail');
+// const { log } = console;
+// const app = express();
 
-const PORT = 3000; 
+// const PORT = 5000; 
 
-//step 2
-//data parsing
-app.use(express.urlencoded({
-    extended: false
-}));
-app.use(express.json());
+// //step 2
+// //data parsing
+// app.use(express.urlencoded({
+//     extended: false
+// }));
+// app.use(express.json());
 
-//email, NomPrénom, text
+// //email, NomPrénom, text
 
-app.post('/email', (req, res) => {
-    const { NomPrénom, email, text } = req.body;
-    log('Data: ', req.body);
+// app.post('/email', (req, res) => {
+//     const { email, text } = req.body;
+//     log('Data: ', req.body);
 
-    sendMail(email, NomPrénom, text, function(err, data) {
-        if (err) {
-            log('ERROR: ', err);
-            return res.status(500).json({ message: err.message || 'Internal Error' });
-        }
-        log('Email sent!!!');
-        return res.json({ message: 'Email sent!!!!!' });
-    });
-});
-
-
-
-app.use( express.static(__dirname + '/'));
-//Render home page
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/index.html"));
-});
+//     sendMail(email, text, function(err, data) {
+//         if (err) {
+//             log('ERROR: ', err);
+//             return res.status(500).json({ message: err.message || 'Internal Error' });
+//         }
+//         log('Email sent!!!');
+//         return res.json({ message: 'Email sent!!!!!' });
+//     });
+// });
 
 
-app.listen (PORT, () => log('Server is starting on PORT,',3000));
+
+// app.use( express.static(__dirname + '/'));
+// //Render home page
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/index.html"));
+// });
+
+
+// app.listen (PORT, () => log('Server is starting on PORT,',5000));
